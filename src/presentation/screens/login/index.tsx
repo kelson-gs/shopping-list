57e9db365d1ed80545bc0e61f54759f2b6d2ac8d
kelson <kelsonredbana12@gmail.com>
2024-04-28 11:35:19 -0300

    tsconfig defined

--- src/presentation/screens/login/index.tsx
+++ src/presentation/screens/login/index.tsx
@@ -6,7 +6,12 @@ import loginStyles from './styles';
 
 const Login = () => {
     return (
-        <LinearGradient style={loginStyles.container}  colors={["rgba(27, 160, 96, 0.69)","rgba(70, 222, 149, 0.87)"]} start={{x:1.5, y:0}} end={{x:0 , y:1}}>
+        <LinearGradient 
+            style={loginStyles.container}  
+            colors={["rgba(27, 160, 96, 0.69)","rgba(70, 222, 149, 0.87)"]} 
+            start={{x:1.5, y:0}} 
+            end={{x:0 , y:1}}
+        >
             <View>
                 <LoginHeader/>
                 <LoginForm />
